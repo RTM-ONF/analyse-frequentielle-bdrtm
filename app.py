f2a554b671517a50edf4c32623d080b5f530e101
clement <clement.roussel@onf.fr>
2026-09-11 18:42:16 +0200

    updates

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ min_year = 1000
 
 max_year = 2026
 
-versions = [
-    "avril 2026"
+echelles = [
+    "Département",
+    "EPCI",
+    "Commune",
+    "Site"
 ]
 
 departements = [
@@ -27,7 +30,9 @@ departements = [
 
 phenomenes = [
     "A (Avalanches)",
+    "E (Ravinement/Ruissellement)",
     "G (Glissements de terrain)",
+    "I (Inondations)",
     "P (Chutes de bloc)",
     "T (Crues torrentielles)"
     ]
@@ -35,7 +40,7 @@ phenomenes = [
 
 @st.cache_data
 def load_data():
-    return pd.read_feather(f"data/{st.session_state.version.replace(" ", "_")}.feather")
+    return pd.read_feather("data/2026_08_17.feather")
 
 st.set_page_config(
     page_title="Analyse fréquentielle de la BDRTM",
@@ -44,11 +49,10 @@ st.set_page_config(
 
 # Widgets communs dans la sidebar
 with st.sidebar:
-    st.header("Extraction de la BDRTM")
+    st.header("Echelle d'analyse")
 
-    st.session_state.version = st.selectbox("Version",
-                                            versions,
-                                            index=0
+    st.session_state.echelle = st.selectbox("Echelle",
+                                            echelles,
                                     )
 
     st.header("Filtres")
@@ -57,7 +61,7 @@ with st.sidebar:
                                                     departements,
                                     )
 
-    st.session_state.annee_min, st.session_state.anne_max = st.slider("Période",
+    st.session_state.annee_min, st.session_state.annee_max = st.slider("Période",
                                                                       min_value=min_year,
                                                                       max_value=max_year,
                                                                       value=(min_year, max_year)
@@ -70,10 +74,7 @@ with st.sidebar:
 
 # Définition des pages
 pg = st.navigation([
-    st.Page("pages/01_accueil.py", title="Accueil"),
-    st.Page("pages/02_analyse_departement.py", title="Analyse à l'échelle du département"),
-    st.Page("pages/03_analyse_site.py", title="Analyse à l'échelle du site"),
-    st.Page("pages/04_analyse_commune.py", title="Analyse à l'échelle de la commune"),
+    st.Page("pages/01_accueil.py", title="Accueil")
 ])
 
 st.session_state.df = load_data()
--- pages/01_accueil.py
+++ pages/01_accueil.py
@@ -1,32 +1,104 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2
 import streamlit as st
+
+def convert_for_download(d):
+    csv = pd.DataFrame(d).to_csv(index=False, sep=";")
+    return csv.encode("utf-8-sig")
 
 st.title("Analyse fréquentielle de la BDRTM")
 
-col1, col2, col3 = st.columns([3,2,3])
-
-st.write(
-    """
-    Placer ici un texte d'introduction / présentation / limites de l'outil...
-    """
-)
+st.header("Données")
 
 df = st.session_state.df
 if st.session_state.departements != []:
-    departements = [int(departement[:2]) for departement in st.session_state.departements]
+    departements = [departement[:2] for departement in st.session_state.departements]
     df = df[df["Dept"].isin(departements)]
 
-df = df[df["an_evt"].between(st.session_state.annee_min, st.session_state.anne_max)]
+df = df[df["an_evt"].between(st.session_state.annee_min, st.session_state.annee_max)]
 
 if st.session_state.phenomenes != []:
     phenomenes = [phenomene[0] for phenomene in st.session_state.phenomenes]
     df = df[df["EV_pheno"].isin(phenomenes)]
 
+if st.session_state.echelle == "Département":
+    df = df[["Dept", "an_evt", "EV_pheno"]]
+
+if st.session_state.echelle == "EPCI":
+    df["epci"] = df["epci"].str.split(",")
+    df = df.explode("epci")
+    df = df[["Dept", "an_evt", "epci", "EV_pheno"]]
+
+if st.session_state.echelle == "Commune":
+    df["SI_communes"] = df["SI_communes"].str.split(";")
+    df = df.explode("SI_communes")
+    df = df[["Dept", "an_evt", "SI_communes", "EV_pheno"]]
+
+if st.session_state.echelle == "Site":
+    df = df[["Dept", "an_evt", "SI_code", "EV_pheno"]]
+
 st.write(df)
 
-st.write(f"{len(df)} entrées.")
+st.write(f"{len(df)} événements.")
 
-with col2:
-    st.image("images/onf.png", width="stretch")
+st.header("Analyse fréquentielle")
+
+echelles = {
+    "Département": "Dept",
+    "EPCI": "epci",
+    "Commune": "SI_communes",
+    "Site": "SI_code"
+}
+
+if st.session_state.phenomenes == []:
+    all_pheno = "_".join(["A", "E", "G", "I", "P", "T"])
+elif len(st.session_state.phenomenes) > 1:
+    all_pheno = "_".join([phenomene[0] for phenomene in st.session_state.phenomenes])
+
+table_1 = df.groupby([echelles[st.session_state.echelle], "EV_pheno"]).size().reset_index(name="nb_evt")
+
+if len(st.session_state.phenomenes) > 1 or st.session_state.phenomenes == []:
+    table_2 = df.groupby(echelles[st.session_state.echelle]).size().reset_index(name="nb_evt")
+    table_2["EV_pheno"] = all_pheno
+    df = pd.concat([table_1, table_2])
+    df.reset_index(drop=True)
+else:
+    df = table_1
+    df.reset_index(drop=True)
+
+T = st.session_state.annee_max - st.session_state.annee_min
+
+df["lambda"] = df["nb_evt"] / T
+df["proba_occ"] = 1. - np.exp(-df["lambda"])
+df["T_retour"] = 1. / df["lambda"]
+df["sigma"] = np.sqrt(df["nb_evt"]) / T
+
+alpha = 0.05
+
+df["IC_inf"] = chi2.ppf(alpha / 2., 2. * df["nb_evt"]) / (2. * T)
+df["IC_sup"] = chi2.ppf(1. - alpha / 2., 2. * (df["nb_evt"] + 1)) / (2. * T)
+
+df["fiabilite"] = np.select(
+    [
+        df["nb_evt"] < 3,
+        df["nb_evt"].between(3, 10, inclusive="both"),
+        df["nb_evt"] > 10
+    ],
+    [1, 2, 3]
+)
+
+fiabilite_table = df["fiabilite"].map({ 1: "faible", 2: "moyenne", 3: "élevée"})
+
+st.write(df)
+
+st.table(fiabilite_table.value_counts())
+
+csv = convert_for_download(df)
+st.download_button(label="Télécharger l'analyse fréquentielle dans un fichier .csv",
+                    data=csv,
+                    file_name="analyse_frequentielle_bdrtm.csv"
+                    )
 
 with st.expander("Avertissement – Clause de non-responsabilité"):
     st.markdown(
